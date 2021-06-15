b57d6ca75115e0a95078b9b7b6c2739a79499699
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-06-15 22:06:48 +0530

    Add files via upload

--- jsonHandler.js
+++ jsonHandler.js
@@ -1,59 +1,6 @@
-function icons() {
-    fetch('json/icons.json')
-    .then(res => res.json())
-    .then(data => {
-
-        for(i=0; i < data.length; i++){
-        let name = data[i].name;
-        let url = data[i].url;
-        let description = data[i].description;
-
-        document.getElementById('icons').innerHTML += `
-        <div class="col" style="display:flex;justify-content: center;">
-        <div class="card" style="width:15rem; height:16rem; margin-bottom:1.5rem; justify-content:space-evenly;">
-          <div class="card-body">
-            <h5 class="card-title" style="font-weight:bold">${name}</h5>
-            <p>${description}</p>
-            <a href="${url}" class="btn btn-primary" target="_blank" style="background-color: #7e6eed">Visit Site</a>
-          </div>
-        </div>
-            </div>
-        `
-        }
-       
-    })
-    .catch(err => console.log(err))
-}
-
-function colors() {
-    fetch("json/colors.json")
-    .then(res => res.json())
-    .then(data => {
-
-        for(i=0; i < data.length; i++){
-        let name = data[i].name;
-        let url = data[i].url;
-        let description = data[i].description;
-
-        document.getElementById("colors").innerHTML += `
-        <div class="col" style="display:flex;justify-content: center;">
-        <div class="card" style="width:15rem; height:16rem; margin-bottom:1.5rem; justify-content:space-evenly;">
-          <div class="card-body">
-            <h5 class="card-title" style="font-weight:bold">${name}</h5>
-            <p>${description}</p>
-            <a href="${url}" target="_blank" class="btn btn-primary" style="background-color: #7e6eed">Visit Site</a>
-          </div>
-        </div>
-            </div>
-        `
-        }
-       
-    })
-    .catch(err => console.log(err))
-}
-
-function illustrations() {
-  fetch("json/illustrations.json")
+function displayList(name) {
+  let id = name;
+  fetch(`json/${id}.json`)
   .then(res => res.json())
   .then(data => {
 
@@ -62,13 +9,13 @@ function illustrations() {
       let url = data[i].url;
       let description = data[i].description;
 
-      document.getElementById("illustrations").innerHTML += `
+      document.getElementById(id).innerHTML += `
       <div class="col" style="display:flex;justify-content: center;">
-      <div class="card" style="width:15rem; height:16rem; margin-bottom:1.5rem; justify-content:space-evenly;">
+      <div class="card" style="width:16rem; height:16rem; margin-bottom:1.5rem; justify-content:space-evenly;">
         <div class="card-body">
           <h5 class="card-title" style="font-weight:bold">${name}</h5>
           <p>${description}</p>
-          <a href="${url}" target="_blank" class="btn btn-primary" style="background-color: #7e6eed">Visit Site</a>
+          <a href="${url}" class="btn btn-primary" target="_blank" style="background-color: #7e6eed">Visit Site</a>
         </div>
       </div>
           </div>
@@ -79,7 +26,7 @@ function illustrations() {
   .catch(err => console.log(err))
 }
 
-////
-icons()
-colors()
-illustrations()
+displayList("icons")
+displayList("colors")
+displayList("illustrations")
+displayList("pab")
