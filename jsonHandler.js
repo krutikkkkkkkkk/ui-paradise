7a6cd490406dd74840bab7773d33c5fb121fdf98
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-06-15 19:06:46 +0530

    Add files via upload

--- jsonHandler.js
+++ jsonHandler.js
@@ -7,7 +7,6 @@ function icons() {
         let name = data[i].name;
         let url = data[i].url;
         let description = data[i].description;
-        console.log(name)
 
         document.getElementById('icons').innerHTML += `
         <div class="col" style="display:flex;justify-content: center;">
@@ -15,7 +14,7 @@ function icons() {
           <div class="card-body">
             <h5 class="card-title" style="font-weight:bold">${name}</h5>
             <p>${description}</p>
-            <a href="${url}" class="btn btn-primary" style="background-color: #7e6eed">Visit Site</a>
+            <a href="${url}" class="btn btn-primary" target="_blank" style="background-color: #7e6eed">Visit Site</a>
           </div>
         </div>
             </div>
@@ -27,7 +26,7 @@ function icons() {
 }
 
 function colors() {
-    fetch('json/icons.json')
+    fetch("json/colors.json")
     .then(res => res.json())
     .then(data => {
 
@@ -35,15 +34,14 @@ function colors() {
         let name = data[i].name;
         let url = data[i].url;
         let description = data[i].description;
-        console.log(name)
 
-        document.getElementById('colors').innerHTML += `
+        document.getElementById("colors").innerHTML += `
         <div class="col" style="display:flex;justify-content: center;">
         <div class="card" style="width:15rem; height:16rem; margin-bottom:1.5rem; justify-content:space-evenly;">
           <div class="card-body">
             <h5 class="card-title" style="font-weight:bold">${name}</h5>
             <p>${description}</p>
-            <a href="${url}" class="btn btn-primary" style="background-color: #7e6eed">Visit Site</a>
+            <a href="${url}" target="_blank" class="btn btn-primary" style="background-color: #7e6eed">Visit Site</a>
           </div>
         </div>
             </div>
@@ -53,5 +51,35 @@ function colors() {
     })
     .catch(err => console.log(err))
 }
+
+function illustrations() {
+  fetch("json/illustrations.json")
+  .then(res => res.json())
+  .then(data => {
+
+      for(i=0; i < data.length; i++){
+      let name = data[i].name;
+      let url = data[i].url;
+      let description = data[i].description;
+
+      document.getElementById("illustrations").innerHTML += `
+      <div class="col" style="display:flex;justify-content: center;">
+      <div class="card" style="width:15rem; height:16rem; margin-bottom:1.5rem; justify-content:space-evenly;">
+        <div class="card-body">
+          <h5 class="card-title" style="font-weight:bold">${name}</h5>
+          <p>${description}</p>
+          <a href="${url}" target="_blank" class="btn btn-primary" style="background-color: #7e6eed">Visit Site</a>
+        </div>
+      </div>
+          </div>
+      `
+      }
+     
+  })
+  .catch(err => console.log(err))
+}
+
+////
 icons()
 colors()
+illustrations()
