b9466c449c342c0d94b2d9f8c768b60a3fa30f9d
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-06-17 22:16:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ window.onload = function(){
     document.getElementById('year').innerHTML = year;
 }
 
+function submitAnotherResponse(){
+    document.getElementById('modal-body').innerHTML = `<label for="name">Name</label>
+    <input type="text" id="name" class="form-control" >
+    <label for="Email">Email</label>
+    <input type="email" id="email" class="form-control">
+    <label for="message">Message</label>
+    <textarea class="form-control" name="message" id="message" cols="10" rows="8"></textarea>`
+
+    document.getElementById('modal-footer').innerHTML = `<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    <button onclick="sendMessage()" type="button" class="btn btn-primary" style="background-color: #7e6eed; border-color: #7e6eed;">Send Message</button>
+  `
+}
+
+
+let url = document.URL;
+
+console.log(url)
+
+
 
 function sendMessage(){
     let name = document.getElementById('name').value;
@@ -11,7 +30,7 @@ function sendMessage(){
     let message = document.getElementById('message').value;
 
     let chat_id = "1171876903"
-    let apiToken= "";
+    let apiToken= "1895204987:AAGP6W_mnk0Jz5uFEoLMVo2Az2aGsVVvA-Y";
     let time = `${d.getHours()}:${d.getMinutes()} IST`
 
     let text = `Name : ${name}
@@ -26,7 +45,7 @@ function sendMessage(){
     .then(
         () => {
             document.getElementById('modal-body').innerHTML = "<h4>Your response was submitted!</h4>"
-            document.getElementById('modal-footer').innerHTML ="<button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>"
+            document.getElementById('modal-footer').innerHTML =" <button onclick='submitAnotherResponse()' type='button' class='btn btn-primary' style='background-color: #7e6eed; border-color: #7e6eed;'>Submit Another Response</button> <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>"
         }
     )
     .catch(err => {
@@ -35,6 +54,13 @@ function sendMessage(){
     })
 }
 
-let url = document.URL;
 
-console.log(url)
+let title = document.getElementById('title');
+
+function randomTitle() {
+    let titles = ["xxxx","Clarify the Problem..!","Be Consistent..!","Be Creative..!","Goal for a WOW Design..!", "Love Your User..!","Be Competitive..!","Don’t Make Personal Opinions..!","Make Things Simple..!","Make Understandable Design..!","Focus on Ease of Use..!"]
+    let randNum = Math.floor((Math.random() * 10) + 1);
+
+    title.innerHTML = titles[randNum];
+}
+randomTitle()
